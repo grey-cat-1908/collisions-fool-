7c17b37d5258940ee5a8e1030ab24ff2d4b11955
Виктор <61203964+grey-cat-1908@users.noreply.github.com>
2024-04-28 21:33:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+let block1, block2;
+let block1Img, block2Img;
+let count = 0;
+
+let m2 = Number(localStorage.getItem("m2")) || 10000;
+let v2 = Number(localStorage.getItem("v2")) || -1;
+let m1 = Number(localStorage.getItem("m1")) || 1;
+let v1 = Number(localStorage.getItem("v1")) || 0;
+
+const opt = 10 ** 6;
+
+function preload() {
+    document.getElementById("m1").value = Number(localStorage.getItem("m1")) || 1;
+    document.getElementById("m2").value = Number(localStorage.getItem("m2")) || 10000;
+    document.getElementById("v1").value = Number(localStorage.getItem("v1")) || 0;
+    document.getElementById("v2").value = Number(localStorage.getItem("v2")) || -1;
+
+    block1Img = loadImage('block1.png');
+    block2Img = loadImage('block2.png');
+}
+
+function setup() {
+    createCanvas(windowWidth, 200, document.getElementById('preview'));
+
+    block1 = new Block(100, 50, m1, v1 / opt, 0, block1Img);
+    block2 = new Block(300, 100, m2, v2 / opt, 50, block2Img);
+
+    countDiv = createDiv("Collisions: " + count);
+    countDiv.class('count')
+
+    v1Div = createDiv("Velocity (Blue): " + block1.velocity * opt);
+    v1Div.class('v1Div')
+
+    v2Div = createDiv("Velocity (Red): " + block2.velocity * opt);
+    v2Div.class('v2Div')
+}
+
+function draw() {
+    background(230);
+  
+    for (let i = 0; i < opt; i++) {
+      if (block1.isCollide(block2)) {
+        v1 = block1.recalcVelocity(block2);
+        v2 = block2.recalcVelocity(block1);
+
+        block1.velocity = v1;
+        block2.velocity = v2;
+        
+        count++;
+      }
+  
+      if (block1.isWall()) {
+        block1.reverse();
+        count++;
+      }
+  
+      block1.move();
+      block2.move();
+    }
+  
+    block1.build();
+    block2.build();
+  
+    countDiv.html("Collisions: " + count);
+    v1Div.html("Velocity (Blue): " + (block1.velocity * opt).toFixed(2));
+    v2Div.html("Velocity (Red): " + (block2.velocity * opt).toFixed(2));
+}
+
+
+addEventListener("input", (event) => {
+    let inputs = document.getElementsByTagName('input');
+
+    for (var i = 0; i < inputs.length; i++) {
+        input = inputs[i];
+        localStorage.setItem(input.id, Number(input.value));
+    }
+});
---
+++ model.js
@@ -0,0 +1,34 @@
+class Block {
+    constructor(coordinate, width, mass, velocity, c1, file) {
+        this.x = coordinate;
+        this.width = width;
+        this.velocity = velocity;
+        this.mass = mass;
+        this.c1 = c1;
+        this.file = file;
+    }
+
+    reverse() {
+        this.velocity *= -1;
+    }
+
+    recalcVelocity(reason) {
+        return ((2 * reason.mass * reason.velocity) + this.velocity * (this.mass - reason.mass)) / (this.mass + reason.mass);
+    }
+
+    isWall() {
+        return this.x <= 0;
+    }
+
+    isCollide(reason) {
+        return !(this.x + this.width < reason.x || this.x > reason.x + reason.width);
+    }
+
+    move() {
+        this.x += this.velocity;
+    }
+
+    build() {
+        image(this.file, constrain(this.x, this.c1, width), height - this.width, this.width, this.width);
+    }
+}
